f65c508f6eb6f87bee16c9e5c682ad2d20872dda
Joey Rocha <jrochaperez123@gmail.com>
2024-02-23 14:30:00 -0800

    3rd commit

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,22 +1,21 @@
-import {mongooseConnect} from "@/lib/mongoose";
-import {Product} from "@/models/Product";
-import {Order} from "@/models/Order";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+import { Order } from "@/models/Order";
 
-
-export default async function handler(req,res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.json('should be a POST request');
     return;
   }
   const {
-    name,email,city,
-    postalCode,streetAddress,country,
+    name, email, city,
+    postalCode, streetAddress, country,
     cartProducts,
   } = req.body;
   await mongooseConnect();
   const productsIds = cartProducts;
   const uniqueIds = [...new Set(productsIds)];
-  const productsInfos = await Product.find({_id:uniqueIds});
+  const productsInfos = await Product.find({ _id: uniqueIds });
 
   let line_items = [];
   for (const productId of uniqueIds) {
@@ -27,7 +26,7 @@ export default async function handler(req,res) {
         quantity,
         price_data: {
           currency: 'USD',
-          product_data: {name:productInfo.title},
+          product_data: { name: productInfo.title },
           unit_amount: quantity * productInfo.price * 100,
         },
       });
@@ -35,26 +34,16 @@ export default async function handler(req,res) {
   }
 
   const orderDoc = await Order.create({
-    line_items,name,email,city,postalCode,
-    streetAddress,country,paid:false,
+    line_items, name, email, city, postalCode,
+    streetAddress, country, paid: false,
   });
-{/* 
 
-  const session = await stripe.checkout.sessions.create({
-    line_items,
-    mode: 'payment',
-    customer_email: email,
-    success_url: process.env.PUBLIC_URL + '/cart?success=1',
-    cancel_url: process.env.PUBLIC_URL + '/cart?canceled=1',
-    metadata: {orderId:orderDoc._id.toString(),test:'ok'},
-  });
-*/
-
-}
-
+  // Simulate Stripe session creation
+  const session = {
+    url: 'https://example.com', // Simulated URL
+  };
 
   res.json({
-    url:session.url,
-  })
-
+    url: session.url,
+  });
 }
