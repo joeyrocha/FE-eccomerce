6f401e404cad9d06e2024377eee98ae410a9abd8
Joey Rocha <jrochaperez123@gmail.com>
2024-02-23 14:22:40 -0800

    secound push

--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,4 @@
 import Header from "@/components/Header";
-import styled from "styled-components";
 import Center from "@/components/Center";
 import {mongooseConnect} from "@/lib/mongoose";
 import {Product} from "@/models/Product";
