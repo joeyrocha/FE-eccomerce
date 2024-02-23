deeedb873e1904d41e4b05b7bbfeaf6348749374
Joey Rocha <jrochaperez123@gmail.com>
2024-02-23 15:15:56 -0800

    3rd commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-          <title>MOSSZILLA</title>
-          {/* other head elements */}
-        </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
